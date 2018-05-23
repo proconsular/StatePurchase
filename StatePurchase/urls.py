9562d7c35888611099b6ad9284381a77c5bbe02b
Justin <justinhu074@icloud.com>
2018-05-23 14:19:12 -0700

    landing templates

--- StatePurchase/urls.py
+++ StatePurchase/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
 from django.conf.urls import include
 
 urlpatterns = [
-    path('', include('apps.game.urls')),
+    url(r'^game/', include('apps.game.urls')),
+    url(r'^main/', include('apps.main.urls')),
 ]
