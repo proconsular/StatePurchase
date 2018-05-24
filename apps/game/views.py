448cc0875f66a7935bdf9b0ab97305463864de92
Jedidiah-Miller <jedidiah.a.miller@gmail.com>
2018-05-23 17:54:45 -0700

    more changes

--- apps/game/views.py
+++ apps/game/views.py
@@ -1,13 +1,2 @@
 from django.shortcuts import render
 from .source.primary.Map import Map
-
-# Create your views here.
-def index(request):
-    map = Map()
-    context = {
-        'map': map.render()
-    }
-    return render(request, 'game/index.html', context)
-
-
-# IGNORE THIS ------ JED
